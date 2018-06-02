915479deace409bbfd7b37abcf34ecee47856d59
Vu Pham <39844697+jamespham8929@users.noreply.github.com>
2018-06-02 12:10:18 -0700

    Create ProductS.cpp

---
+++ ProductS.cpp
@@ -0,0 +1,67 @@
+/*
+ * ProductS.cpp
+ *
+ *  Created on: May 30, 2018
+ *      Author: vupham
+ */
+
+#include "ProductS.h"
+#include <iostream>
+using namespace std;
+
+ProductS::ProductS() {
+	make = "";
+	model = "";
+	screenSize = 0.0;
+	cpuGen = 0;
+	year = 0;
+}
+
+ProductS::ProductS(string make, string model, double screenSize, unsigned cpuGen, unsigned year, double price) {
+	this->make = make;
+	this->model = model;
+	this->screenSize = screenSize;
+	this->cpuGen = cpuGen;
+	this->year = year;
+	this->price = price;
+}
+
+void ProductS::operator=(const Product& product) {
+	make = product.getMake();
+	model = product.getModel();
+	screenSize = product.getScreenSize();
+	cpuGen = product.getCpuGen();
+	year = product.getYear();
+	price = product.getPrice();
+}
+
+bool ProductS::operator==(const ProductS& product) {
+	return (model == product.model && make == product.make);
+}
+
+bool ProductS::operator<(const ProductS& product) {
+	if (model < product.model)
+		return true;
+	else if (model == product.model) {
+		if (make < product.make)
+			return true;
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+bool ProductS::operator>(const ProductS& product) {
+	if (model > product.model)
+		return true;
+	else if (model == product.model) {
+		if (make > product.make)
+			return true;
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
