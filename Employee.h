ecb46036171230505438e25e37a5bf11b80c19f8
Vu Pham <39844697+jamespham8929@users.noreply.github.com>
2018-06-07 17:03:11 -0700

    Create Employee.h

---
+++ Employee.h
@@ -0,0 +1,34 @@
+/*
+ * User.h
+ *
+ *  Created on: June 7, 2018
+ *      Author: vupham
+ */
+
+#ifndef EMPLOYEE_H_
+#define EMPLOYEE_H_
+
+#include "User.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee : public User {
+public:
+	Employee();
+	Employee(string username, string password, string firstName, string lastName, bool isEmployee);
+
+	//check firstname -> lastname -> username -> password
+	bool operator==(const Employee& employee);
+	bool operator<(const Employee& employee);
+	bool operator>(const Employee& employee);
+
+	void read(ifstream& in);		//read data from file
+	void write(ostream& out);	//write data to file
+
+	friend ostream& operator<<(ostream& out, const Employee& employee);		//display in console
+};
+
+
+
+#endif /* EMPLOYEE_H_ */
